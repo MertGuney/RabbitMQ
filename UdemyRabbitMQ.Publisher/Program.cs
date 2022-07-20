4f9122e304f4daa0861f0d0b5acc8e0f0194573b
Mert Güney <mert_guney_29@hotmail.com>
2022-07-20 10:02:57 +0300

    Initial

---
+++ UdemyRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,98 @@
+﻿using RabbitMQ.Client;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace UdemyRabbitMQ.Publisher
+{
+    public enum LogNames
+    {
+        Critical = 1,
+        Error,
+        Warning,
+        Info
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://okdmnwkm:LqWuSfuY66v1aZXnjHub5bBb-rWi4YKK@cattle.rmq2.cloudamqp.com/okdmnwkm");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            // 2. prop false olursa hafızada(memory) tutulur restart atarsa tüm kuyruk gider true olursa fiziksel kaydedilir restart atılsa bile gitmez
+            // 3. prop true olursa sadece burada oluşturduğumuz kanal üzerinden bağlanabiliriz false olursa sub üzerinden farklı bir kanaldan bağlanabiliriz
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            //channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+            //channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+            //channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+            //Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            //{
+
+
+
+            //    var routeKey = $"route-{x}";
+            //    var queueName = $"direct-queue-{x}";
+            //    channel.QueueDeclare(queueName, true, false, false);
+            //    channel.QueueBind(queueName, "logs-direct", routeKey, null);
+            //});
+
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{
+            //    //LogNames log = (LogNames)new Random().Next(1, 5);
+            //    //string message = $"Message {x}";
+            //    //string message = $"Log {x}";
+            //    //string message = $"Log-Type: {log}";
+
+
+            //    Random rnd = new Random();
+            //    LogNames log1 = (LogNames)rnd.Next(1, 5);
+            //    LogNames log2 = (LogNames)rnd.Next(1, 5);
+            //    LogNames log3 = (LogNames)rnd.Next(1, 5);
+
+            //    var routeKey = $"{log1}.{log2}.{log3}";
+            //    string message = $"Log-Type: {log1}-{log2}-{log3}";
+            //    var messageBody = Encoding.UTF8.GetBytes(message);
+            //    //channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+            //    //channel.BasicPublish("logs-fanout", string.Empty, null, messageBody);
+
+            //    //var routeKey = $"route-{log}";
+            //    //channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+
+            //    channel.BasicPublish("logs-topic", routeKey, null, messageBody);
+
+
+            //    //Console.WriteLine($"Mesaj Gönderilmiştir : {message}");
+            //    Console.WriteLine($"Log Gönderilmiştir : {message}");
+            //});
+
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+
+            var properties = channel.CreateBasicProperties();
+            properties.Headers = headers;
+            properties.Persistent = true;//mesajlarda kalıcı hale gelir
+
+
+            var product = new Product { Id = 1, Name = "Kalem", Price = 100, Stock = 10 };
+            var productJsonString = JsonSerializer.Serialize(product);
+
+
+            //channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes("Header Mesajım"));
+            channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes(productJsonString));
+            Console.WriteLine("Mesaj Gönderilmiştir.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,74 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+namespace UdemyRabbitMQ.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://okdmnwkm:LqWuSfuY66v1aZXnjHub5bBb-rWi4YKK@cattle.rmq2.cloudamqp.com/okdmnwkm");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //var randomQueueName =channel.QueueDeclare().QueueName;
+            //channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
+            // 3. prop true olursa 6 mesaj varsa sublara böler (2 sub varsa her biri 3er tane mesaj alır) false olursa belirtilen mesaj sayısı kadar (2.propta belirtiyoruz) her sub'a gönderir.   
+            channel.BasicQos(0, 1, false);
+            // bu satır kalırsa publisherda kuyruk olsa da olmasada hata almaz kendi oluşturur 
+            // iki taraftada aynı parametreleri vermeliyiz
+            //channel.QueueDeclare("hello-queue", true, false, false);
+            var consumer = new EventingBasicConsumer(channel);
+            // 2. prop true olursa kuyuruktan direkt siler false olursa biz sonradan silme emri veririz
+            //channel.BasicConsume("hello-queue", false, consumer);
+
+            Console.WriteLine("Console Dinleniyor...");
+
+            //var routeKey = "*.Error.*";//ortasındaki ifade error olan 
+            //var routeKey = "*.*.Warning";//sonundaki ifade warning olan 
+            //var routeKey = "Info.#";//Başındaki ifade Info gerisi önemsiz olan 
+
+            //var queueName = "direct-queue-Critical";
+            var queueName = channel.QueueDeclare().QueueName;
+
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+            headers.Add("x-match", "all");
+
+            //channel.QueueBind(queueName, "logs-topic", routeKey);
+            channel.QueueBind(queueName, "header-exchange", string.Empty, headers);
+
+            //channel.BasicConsume(randomQueueName, false, consumer);
+
+            channel.BasicConsume(queueName, false, consumer);
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+              {
+                  var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                  Product product = JsonSerializer.Deserialize<Product>(message);
+
+                  Thread.Sleep(1000);
+                  //Console.WriteLine("Gelen Mesaj: " + message);
+                  Console.WriteLine($"Gelen Mesaj: + {product.Id }-{ product.Name}-{product.Price}-{product.Stock}");
+                  //File.AppendAllText("log-critical.txt", message + "\n");
+
+                  channel.BasicAck(e.DeliveryTag, false);
+              };
+
+
+            Console.ReadLine();
+        }
+    }
+}
